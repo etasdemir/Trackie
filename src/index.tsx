88e46de502aa32e220886efa100337f27cdc1adb
etasdemir <erentasdemircs@gmail.com>
2022-07-12 22:39:19 +0300

    start project

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+import styled from 'styled-components';
+
+function App() {
+  return <Temp>HELLO</Temp>;
+}
+
+const Temp = styled(Text)``;
+
+export default App;
