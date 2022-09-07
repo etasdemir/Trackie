ca63711acff6bab53d0b72874616807af47ecc0c
etasdemir <erentasdemircs@gmail.com>
2022-09-07 21:22:02 +0300

    update app index

--- index.tsx
+++ index.tsx
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry, Platform, UIManager} from 'react-native';
-import App from 'src';
+import App from './src';
 import {name as appName} from './app.json';
 
 if (
